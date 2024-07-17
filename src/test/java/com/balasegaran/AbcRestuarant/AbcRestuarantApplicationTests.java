31d7c4b9fc4c1afff1445016a90c6eb2e98039ab
S Segaran <blsgrn@tutanota.com>
2024-07-17 21:50:43 +0530

    Intial commit

---
+++ src/test/java/com/balasegaran/AbcRestuarant/AbcRestuarantApplicationTests.java
@@ -0,0 +1,13 @@
+package com.balasegaran.AbcRestuarant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AbcRestuarantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
