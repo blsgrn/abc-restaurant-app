54ffeaafd1246cbef797c4cb3a8c6c635d4846df
S Segaran <blsgrn@tutanota.com>
2024-07-21 16:29:00 +0530

    Add Config file to convert String in url to ObjectId

---
+++ src/main/java/com/balasegaran/AbcRestuarant/Config/StringToObjectIdConverter.java
@@ -0,0 +1,13 @@
+package com.balasegaran.AbcRestuarant.Config;
+
+import org.bson.types.ObjectId;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.stereotype.Component;
+
+@Component
+public class StringToObjectIdConverter implements Converter<String, ObjectId> {
+  @Override
+  public ObjectId convert(String source) {
+    return new ObjectId(source);
+  }
+}
